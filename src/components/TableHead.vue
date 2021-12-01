c0c2706cfb23746d07e38beab7a8fe8d5c166c23
Katerina Tikhonova <missoctober390@gmail.com>
2021-12-01 20:55:06 +0300

    Task-6: Basic table showing

---
+++ src/components/TableHead.vue
@@ -0,0 +1,15 @@
+<template>
+  <th>{{ head }}</th>
+</template>
+
+<script>
+export default {
+  name: "TableHead",
+  props: {
+    head: {
+      default: "",
+      type: String,
+    },
+  },
+};
+</script>
